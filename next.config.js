4471956fdec23d4154eb8aa50f3695ef15ac1f54
wassim <wassimbenjdida@gmail.com>
2023-04-08 11:43:48 +0100

    moved

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["placeimg.com", "mobirise.com"],
-  },
-};
-
-module.exports = nextConfig;
